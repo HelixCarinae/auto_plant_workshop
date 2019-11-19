194ef5fff20d5ae58a881faae197f17b6a9d8416
Josh Watson <15268766+HelixCarinae@users.noreply.github.com>
2019-11-18 20:08:31 -0400

    added board_test

---
+++ arduino/board_test/board_test.ino
@@ -0,0 +1,36 @@
+#define L_PIN     9  // Pin for Left LEDs
+#define M_PIN     10 // Pin for Middle LEDs
+#define R_PIN     11 // Pin for Right LEDs
+#define PUMP      3  // Pin to control PUMP
+#define SNSR_PWR  13 // Pin to turn on power to the sensor to take readings
+#define SNSR      A0 // Pin for hygrometer (moisture sensor)
+
+void setup() {
+  // Seting up pins to used as outputs to turn on MOSFETS
+  pinMode(L_PIN, OUTPUT);
+  pinMode(M_PIN, OUTPUT);
+  pinMode(R_PIN, OUTPUT);
+  pinMode(PUMP, OUTPUT);
+  pinMode(SNSR_PWR, OUTPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(SNSR_PWR, HIGH);
+  delay(20);
+  Serial.println(analogRead(SNSR));
+  delay(20);
+  digitalWrite(SNSR_PWR, LOW);
+  
+  delay(1000);
+  digitalWrite(L_PIN, HIGH);
+  digitalWrite(M_PIN, HIGH);
+  digitalWrite(R_PIN, HIGH);
+
+  delay(1000);
+  digitalWrite(L_PIN, LOW);
+  digitalWrite(M_PIN, LOW);
+  digitalWrite(R_PIN, LOW);
+}
